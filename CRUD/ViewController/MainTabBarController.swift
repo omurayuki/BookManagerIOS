d85951c0e7b0b5ad96c2834dee69013d5952c8e4
y.omura <y.omura@caraquri.com>
2019-01-16 18:47:33 +0900

    書籍一覧とログアウトのタブ機能

---
+++ CRUD/ViewController/MainTabBarController.swift
@@ -0,0 +1,25 @@
+import Foundation
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    private var booklistViewController: BookListViewController!
+    private var logoutViewController: LogoutViewController!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setTabBar()
+    }
+}
+
+extension MainTabBarController {
+    private func setTabBar() {
+        booklistViewController = BookListViewController()
+        booklistViewController.tabBarItem = UITabBarItem(tabBarSystemItem: .featured, tag: 1)
+        booklistViewController.tabBarItem = UITabBarItem(title: "一覧", image: nil, tag: 1)
+        logoutViewController = LogoutViewController()
+        logoutViewController.tabBarItem = UITabBarItem(tabBarSystemItem: .contacts, tag: 2)
+        logoutViewController.tabBarItem = UITabBarItem(title: "設定", image: nil, tag: 2)
+        let myTabs = NSArray(objects: booklistViewController, logoutViewController)
+        self.setViewControllers(myTabs as? [UIViewController], animated: false)
+    }
+}
